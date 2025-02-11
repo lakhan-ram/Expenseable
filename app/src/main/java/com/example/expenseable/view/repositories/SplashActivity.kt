038bdcccaeefc357cc87abe98918370c71376c8e
Ram Lakhan <r04ramlakhan@gmail.com>
2025-02-11 22:41:01 +0530

    add splash screen

---
+++ app/src/main/java/com/example/expenseable/view/repositories/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.example.expenseable.view.repositories
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.expenseable.R
+import com.example.expenseable.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_text)
+        binding.tvAppTitle.startAnimation(slideAnimation)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, SignInActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
