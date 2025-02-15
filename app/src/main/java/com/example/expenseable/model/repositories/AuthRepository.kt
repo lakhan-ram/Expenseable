16c5aab29c899bc8e69c7cd19ed8060acce2ba85
Ram Lakhan <r04ramlakhan@gmail.com>
2025-02-15 11:04:59 +0530

    add user authentication feature

---
+++ app/src/main/java/com/example/expenseable/model/repositories/AuthRepository.kt
@@ -0,0 +1,59 @@
+package com.example.expenseable.model.repositories
+
+import android.content.Context
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import com.example.expenseable.model.entities.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+
+class AuthRepository(private val context: Context) {
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val firebaseReference = FirebaseDatabase.getInstance().getReference("users")
+    val authList: MutableLiveData<FirebaseUser> = MutableLiveData<FirebaseUser>()
+
+    fun signUp(name: String, email: String, password: String) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { taskSignUp ->
+                if (taskSignUp.isSuccessful) {
+                    val id = firebaseAuth.currentUser?.uid.toString()
+                    val user = User(id, name, email)
+                    firebaseReference.child(id).setValue(user)
+                        .addOnCompleteListener { taskCreateUser ->
+                            if (taskCreateUser.isSuccessful) {
+                                authList.postValue(firebaseAuth.currentUser)
+                            } else {
+                                firebaseAuth.currentUser?.delete()
+                                Toast.makeText(
+                                    context,
+                                    "Error: ${taskCreateUser.exception?.localizedMessage}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                } else {
+                    Toast.makeText(
+                        context,
+                        "Error: ${taskSignUp.exception?.localizedMessage}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    fun signIn(email:String, password:String){
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { taskSignIn ->
+                if (taskSignIn.isSuccessful) {
+                    authList.postValue(firebaseAuth.currentUser)
+                } else {
+                    Toast.makeText(
+                        context,
+                        "Error: ${taskSignIn.exception?.localizedMessage}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+}
--- app/src/main/java/com/example/expenseable/view/repositories/SignInActivity.kt
+++ app/src/main/java/com/example/expenseable/view/repositories/SignInActivity.kt
@@ -1,21 +1,61 @@
 package com.example.expenseable.view.repositories
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.expenseable.R
+import com.example.expenseable.databinding.ActivitySignInBinding
+import com.example.expenseable.viewmodel.AuthViewModel
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var authViewModel: AuthViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        authViewModel = AuthViewModel(this)
+
+        authViewModel.authList.observe(this) {
+            if (it != null) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finishAffinity()
+            }
+        }
+        binding.tvSignUpSignIn.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnSignInSignIn.setOnClickListener {
+            val email = binding.etTextEmailSignIn.text.toString()
+            val password = binding.etTextPasswordSignIn.text.toString()
+
+            if (email.isEmpty() && password.isEmpty()) {
+                binding.textInputLayoutEmailSignIn.error = "Email is required"
+                binding.textInputLayoutPasswordSignIn.error = "Password is required"
+            }
+            if (email.isEmpty()) {
+                binding.textInputLayoutEmailSignIn.error = "Email is required"
+            }
+            if (password.isEmpty()) {
+                binding.textInputLayoutPasswordSignIn.error = "Password is required"
+            }
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                authViewModel.signIn(email, password)
+                binding.progressBarSignIn.visibility = View.VISIBLE
+            }
         }
     }
 }
